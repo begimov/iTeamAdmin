71b35c9e361fe8b567d62a9221d386a631e22a7c
begimov <begimov@gmail.com>
2017-08-15 14:14:15 +0300

    sass variables and vue component check

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,19 +4,7 @@
 <div class="container">
   <div class="row">
     <div class="col-md-12">
-      <div class="jumbotron">
-        <div class="row">
-          <div class="col-md-4">
-            Изображение/Видео
-          </div>
-          <div class="col-md-8">
-            <h1>Заголовок</h1>
-            <p class="lead">Подзаголовок</p>
-            <p>Описание</p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">CTA Кнопка</a></p>
-          </div>
-        </div>
-      </div>
+      Home
     </div>
   </div>
 </div>
