e1b2ba9b96442fbc462d9b10892c09d0c935ba7d
begimov <begimov@gmail.com>
2017-08-15 13:49:06 +0300

    basic layouts architecture added

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      FOOTER
+    </div>
+  </div>
+</div>
---
+++ resources/views/layouts/partials/_header.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      HEADER
+    </div>
+  </div>
+</div>
