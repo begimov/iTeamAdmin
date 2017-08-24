45706c93cf9683ebb708bf5a42bbf4df28b45ba0
begimov <begimov@gmail.com>
2017-08-24 14:46:25 +0300

    user roles relationship added

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Auth\Role;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'users_roles');
+    }
 }
