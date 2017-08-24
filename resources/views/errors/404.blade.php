d2a15e8ef59018dc77930116d6522dfc96af4070
begimov <begimov@gmail.com>
2017-08-24 15:13:13 +0300

    custom 404 template added

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-heading"><h4>Страница не найдена</h4></div>
+        </div>
+    </div>
+  </div>
+</div>
+@endsection
