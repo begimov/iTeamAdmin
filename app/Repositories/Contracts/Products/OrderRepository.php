743190a0e72f7f93aa2d82dec0aa694d69d2d43c
begimov <begimov@gmail.com>
2017-08-29 14:06:15 +0300

    contract and implementation for order repository added

---
+++ app/Repositories/Contracts/Products/OrderRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\Contracts\Products;
+
+interface OrderRepository
+{
+    public function getAll();
+}
---
+++ app/Repositories/Eloquent/Products/EloquentOrderRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Eloquent\Products;
+
+use App\Repositories\Contracts\Products\OrderRepository;
+use App\Models\Products\Order;
+
+class EloquentOrderRepository implements OrderRepository
+{
+
+    public function getAll()
+    {
+        //
+    }
+
+}
