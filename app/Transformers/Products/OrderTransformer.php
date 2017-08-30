84573d94892beea30a51bb838dd041835240022e
begimov <begimov@gmail.com>
2017-08-30 11:40:17 +0300

    User added to OrderTransformer as available include

--- app/Transformers/Products/OrderTransformer.php
+++ app/Transformers/Products/OrderTransformer.php
@@ -3,14 +3,22 @@
 namespace App\Transformers\Products;
 
 use App\Models\Products\Order;
+use App\Transformers\UserTransformer;
 
 class OrderTransformer extends \League\Fractal\TransformerAbstract
 {
+    protected $availableIncludes = ['user'];
+
     public function transform(Order $order)
     {
         return [
             'id' => $order->id,
             'created_at_human' => $order->created_at->diffForHumans(),
         ];
+    }
+
+    public function includeUser(Order $order)
+    {
+        return $this->item($order->user, new UserTransformer);
     }
 }
