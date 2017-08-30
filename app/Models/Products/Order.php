4d0acecef0e122f1c5c4dcf374ef66e189b80c35
begimov <begimov@gmail.com>
2017-08-30 12:11:22 +0300

    order-user relationship added to order model

--- app/Models/Products/Order.php
+++ app/Models/Products/Order.php
@@ -3,9 +3,15 @@
 namespace App\Models\Products;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Order extends Model
 {
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function scopeLatestFirst($query)
     {
         return $query->latest();
