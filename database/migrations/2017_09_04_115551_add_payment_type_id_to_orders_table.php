0f64943445ddc1ac8b187cf82f5f2f84823543f2
begimov <begimov@gmail.com>
2017-09-04 16:52:38 +0300

    migrations to add payment_type_id and payment_state_id added

---
+++ database/migrations/2017_09_04_115551_add_payment_type_id_to_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPaymentTypeIdToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('payment_type_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('payment_type_id');
+        });
+    }
+}
