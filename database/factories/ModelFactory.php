e785eb8e08f57eb2e2eae2840ea1fec7ee3e14da
begimov <begimov@gmail.com>
2017-09-06 17:34:49 +0300

    new factories for models added

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,6 +12,8 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+// 1 me and test with admin role + some
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
@@ -23,14 +25,42 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(App\Models\Landings\Landing::class, function (Faker\Generator $faker) {
+// 2
+$factory->define(App\Models\Products\Product::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->sentence,
+        'price' => $faker->numberBetween($min = 400, $max = 4000),
     ];
 });
 
+// 3 for each user ->create(['user_id' => ?])
+$factory->define(App\Models\Users\UserProfile::class, function (Faker\Generator $faker) {
+    return [
+        'phone' => $faker->e164PhoneNumber,
+    ];
+});
+
+// 4
 $factory->define(App\Models\Products\Order::class, function (Faker\Generator $faker) {
     return [
-        //
+        'user_id' => function () {
+            return App\User::inRandomOrder()->first()->id;
+        },
+        // not paid => no payment_type_id ???
+        'payment_type_id' => $faker->numberBetween($min = 1, $max = 4),
+        'payment_state_id' => $faker->numberBetween($min = 1, $max = 3),
+        'product_id' => function () {
+            return App\Models\Products\Product::inRandomOrder()->first()->id;
+        }
+    ];
+});
+
+
+
+
+
+$factory->define(App\Models\Landings\Landing::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->sentence,
     ];
 });
