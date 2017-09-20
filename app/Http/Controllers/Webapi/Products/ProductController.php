c8fe78a8ff2dca3033d10ff0733e2170ea5ec7d0
begimov <begimov@gmail.com>
2017-09-20 11:55:30 +0300

    products blade root template created and returned via index method in products webapi controller

--- app/Http/Controllers/Webapi/Products/ProductController.php
+++ app/Http/Controllers/Webapi/Products/ProductController.php
@@ -26,7 +26,7 @@ class ProductController extends Controller
      */
     public function index(Request $request)
     {
-        echo "string";
+        return view('products.index');
     }
 
     /**
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+  <div class="row">
+    <div class="col-md-12">
+      PRODUCTS
+    </div>
+  </div>
+
+</div>
+@endsection
