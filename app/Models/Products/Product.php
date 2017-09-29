36b12aa52123f0addb121d9f2306c23cbcf1f8b0
begimov <begimov@gmail.com>
2017-09-29 12:31:17 +0300

    softdeletes trait and property added to product model

--- app/Models/Products/Product.php
+++ app/Models/Products/Product.php
@@ -9,6 +9,15 @@ use App\Models\Products\PriceTag;
 
 class Product extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
