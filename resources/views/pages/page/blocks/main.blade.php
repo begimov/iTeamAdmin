69f9b54b0ebaab9beea9cdce6770a76bfd031d13
begimov <begimov@gmail.com>
2017-10-02 13:09:26 +0300

    add_data_to_contents_table migration created

--- resources/views/pages/page/blocks/main.blade.php
+++ resources/views/pages/page/blocks/main.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
   MAIN.BLADE
   <p id="inode_12">{{ $element->contents->where('node_id', 'inode_12')->first()->created_at }}</p>
-  <p id="inode_13">{{ $element->contents->where('node_id', 'inode_13')->first()->created_at }}</p>
+  <a id="inode_13" href="{{ $element->contents->where('node_id', 'inode_13')->first()->created_at }}">Link</a></p>
 </div>
