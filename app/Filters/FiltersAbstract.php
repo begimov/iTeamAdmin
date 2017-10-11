be2ed5d3851a7c18efb12821cf2f0a00773f0591
begimov <begimov@gmail.com>
2017-10-11 08:23:35 +0300

    instantiating only those filter classe which query params are presented in request and are not empty

--- app/Filters/FiltersAbstract.php
+++ app/Filters/FiltersAbstract.php
@@ -15,13 +15,24 @@ class FiltersAbstract
 
     public function filter($builder)
     {
-        foreach ($this->filters as $filter => $class) {
-            $this->resolveFilter($class);
+        foreach ($this->getFilters() as $filter => $class) {
+            var_dump($this->resolveFilter($filter));
         }
+        return $builder;
     }
 
-    protected function resolveFilter($class)
+    protected function resolveFilter($filter)
     {
-        return new $class;
+        return new $this->filters[$filter];
+    }
+
+    protected function getFilters()
+    {
+        return $this->filterFilters($this->filters);
+    }
+
+    protected function filterFilters($filters)
+    {
+        return array_filter($this->request->only(array_keys($filters)));
     }
 }
