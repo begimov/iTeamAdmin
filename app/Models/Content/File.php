75db3059259a9544d911fe235fae8cbf25449a56
begimov <begimov@gmail.com>
2017-10-30 05:50:18 +0300

    soft deleting set up in file model

--- app/Models/Content/File.php
+++ app/Models/Content/File.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
