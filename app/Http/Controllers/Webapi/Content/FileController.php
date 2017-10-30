0ff1e797a95375bb7b84133445603051af43c595
begimov <begimov@gmail.com>
2017-10-30 07:42:43 +0300

    Content\FileController created

---
+++ app/Http/Controllers/Webapi/Content/FileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Webapi\Content;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FileController extends Controller
+{
+    //
+}
