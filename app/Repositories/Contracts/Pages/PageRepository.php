5ebf2d063eb82abee4fa12bd081f10ff8884ba40
begimov <begimov@gmail.com>
2017-11-01 11:27:11 +0300

    extending from EloquentRepositoryAbstract and entity method added to repositories, filter methods modified respectively

--- app/Repositories/Contracts/Pages/PageRepository.php
+++ app/Repositories/Contracts/Pages/PageRepository.php
@@ -4,6 +4,5 @@ namespace App\Repositories\Contracts\Pages;
 
 interface PageRepository
 {
-  public function filter($request);
   public function destroyById($id);
 }
--- app/Repositories/Contracts/Products/ProductRepository.php
+++ app/Repositories/Contracts/Products/ProductRepository.php
@@ -4,6 +4,5 @@ namespace App\Repositories\Contracts\Products;
 
 interface ProductRepository
 {
-    public function filter($request);
     public function destroyById($id);
 }
