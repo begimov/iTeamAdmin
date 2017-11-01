8296d3b804743ef06c71a10746066b5ed0f97a06
begimov <begimov@gmail.com>
2017-11-01 08:31:29 +0300

    EloquentProductRepository now extends EloquentRepositoryAbstract

--- app/Repositories/Eloquent/Products/EloquentProductRepository.php
+++ app/Repositories/Eloquent/Products/EloquentProductRepository.php
@@ -2,11 +2,12 @@
 
 namespace App\Repositories\Eloquent\Products;
 
+use App\Repositories\EloquentRepositoryAbstract;
 use App\Repositories\Contracts\Products\ProductRepository;
 
 use App\Models\Products\Product;
 
-class EloquentProductRepository implements ProductRepository
+class EloquentProductRepository extends EloquentRepositoryAbstract implements ProductRepository
 {
     public function filter($request)
     {
