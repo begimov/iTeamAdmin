49c8cba6a6db2b558d1d3318a65c88c038b558f1
begimov <begimov@gmail.com>
2017-11-17 07:04:35 +0300

    hasName global query scope added to Material model to get only once saved materials, not empty initially created ones

--- app/Models/Products/Material.php
+++ app/Models/Products/Material.php
@@ -3,10 +3,25 @@
 namespace App\Models\Products;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use App\Models\Content\File;
 
 class Material extends Model
 {
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('hasName', function (Builder $builder) {
+            $builder->whereNotNull('name');
+        });
+    }
+
     public function files()
     {
         return $this->hasMany(File::class);
