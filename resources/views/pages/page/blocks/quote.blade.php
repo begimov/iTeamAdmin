52f759d160f596ddd1c6e9db2e52b644e983039f
begimov <begimov@gmail.com>
2017-12-13 13:53:19 +0300

    quote block created

---
+++ resources/views/pages/page/blocks/quote.blade.php
@@ -0,0 +1,18 @@
+<?php // TODO: how to store and show pages / elements /blocks ?>
+@if (isset($page))
+<div class="row">
+  <div class="col">
+    <blockquote>
+      {{ getElementContentData($element, 'quote') }}
+    </blockquote>
+  </div>
+</div>
+@else
+  <div class="row">
+    <div class="col-md-12">
+      <blockquote>
+        <textarea cols="30" rows="4" class="form-control" v-model="quote"></textarea>
+      </blockquote>  
+    </div>
+  </div>
+@endif
