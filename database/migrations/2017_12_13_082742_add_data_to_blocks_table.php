2d02d54b14bd416dd39234aed3d903dc9d6026d9
begimov <begimov@gmail.com>
2017-12-13 11:45:21 +0300

    migration to add data column for block components data props list addded

---
+++ database/migrations/2017_12_13_082742_add_data_to_blocks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDataToBlocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blocks', function (Blueprint $table) {
+            $table->string('data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blocks', function (Blueprint $table) {
+            $table->dropColumn('data');
+        });
+    }
+}
