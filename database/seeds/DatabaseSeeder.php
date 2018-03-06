acbcadcbe17e8a0217d9c5f77c9a21bc2b5194d0
begimov <begimov@gmail.com>
2018-03-06 14:01:36 +0300

    seeders refactored and updated, new ones created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(BusinessEntitiesTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(PaymentStatesTableSeeder::class);
+        $this->call(PaymentTypesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(BlocksTableSeeder::class);
     }
 }
