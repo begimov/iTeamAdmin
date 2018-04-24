414d2092d855b3dde429e87cbd55b412fe471668
begimov <begimov@gmail.com>
2018-04-24 12:15:33 +0300

    RelationsServiceProvider's boot method implemented

--- app/Providers/RelationsServiceProvider.php
+++ app/Providers/RelationsServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class RelationsServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class RelationsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Relation::morphMap([
+            'materials' => 'App\Models\Products\Material',
+        ]);
     }
 
     /**
