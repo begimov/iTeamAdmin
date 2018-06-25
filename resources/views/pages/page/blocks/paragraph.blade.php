e99e7611e232cb7cbdb8cf3217f1ec828aac0217
begimov <begimov@gmail.com>
2018-06-25 11:31:48 +0300

    displaying card checkbox has been added to paragraph block's template

--- resources/views/pages/page/blocks/paragraph.blade.php
+++ resources/views/pages/page/blocks/paragraph.blade.php
@@ -2,6 +2,10 @@
   <div class="col-md-12">
     <p>
       <quill-editor v-model="data.text"></quill-editor>
+      <div class="form-check" style="margin:10px 0;">
+        <input type="checkbox" id="isCardCheckbox" v-model="data.isCard" class="form-check-input">
+        <label for="isBundleCheckbox" class="form-check-label">Отображать текст на карточке</label>
+      </div>
     </p>  
   </div>
 </div>
