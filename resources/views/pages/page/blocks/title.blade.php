7a26bb0d082dc787046c701180ab746cc3556ab2
begimov <begimov@gmail.com>
2018-06-25 12:56:46 +0300

    toolbar font color module has been added to quill config in title block's template

--- resources/views/pages/page/blocks/title.blade.php
+++ resources/views/pages/page/blocks/title.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-md-12">
     <p>
-      <quill-editor v-model="data.text" :options="{modules: {toolbar: [[{ 'align': [] }]]}}"></quill-editor>
+      <quill-editor v-model="data.text" :options="{modules: {toolbar: [[{ 'align': [] }],[{ 'color': [] }]]}}"></quill-editor>
     </p>  
   </div>
 </div>
