a44d613648f85f466a28284e0ef9e9262b9d743e
begimov <begimov@gmail.com>
2018-06-25 12:10:06 +0300

    temporary fix for inputs with same ids has been implemented

--- resources/views/pages/page/blocks/paragraph.blade.php
+++ resources/views/pages/page/blocks/paragraph.blade.php
@@ -3,8 +3,8 @@
     <p>
       <quill-editor v-model="data.text"></quill-editor>
       <div class="form-check" style="margin:10px 0;">
-        <input type="checkbox" id="isCardCheckbox" v-model="data.isCard" class="form-check-input">
-        <label for="isBundleCheckbox" class="form-check-label">Отображать текст на карточке</label>
+        <input type="checkbox" v-model="data.isCard" class="form-check-input">
+        <label class="form-check-label">Отображать текст на карточке</label>
       </div>
     </p>  
   </div>
