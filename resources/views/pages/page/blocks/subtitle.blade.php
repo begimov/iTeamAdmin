6511c730826ff9db3744ea8f4606df769f1f51fe
begimov <begimov@gmail.com>
2018-06-27 14:33:29 +0300

    font color picker added to subtitle block's template

--- resources/views/pages/page/blocks/subtitle.blade.php
+++ resources/views/pages/page/blocks/subtitle.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-md-12">
     <p>
-      <quill-editor v-model="data.text" :options="{modules: {toolbar: [['italic'],[{ 'align': [] }]]}}"></quill-editor>
+      <quill-editor v-model="data.text" :options="{modules: {toolbar: [['italic'],[{ 'align': [] }],[{ 'color': [] }]]}}"></quill-editor>
     </p>  
   </div>
 </div>
