40e20516d916a1a22ef9dde5eeb66af391ea79c5
begimov <begimov@gmail.com>
2018-07-08 10:14:03 +0300

    TextReviews component's layout has been updated

--- public/js/app.js
+++ public/js/app.js
@@ -66163,6 +66163,12 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
     }, [_vm._v(_vm._s('Отзыв ' + (index + 1)))]), _vm._v(" "), _c('div', {
       staticClass: "panel-body"
     }, [_c('div', {
+      staticClass: "row"
+    }, [_c('div', {
+      staticClass: "col-sm-2"
+    }, [_vm._v("\n                                //\n                            ")]), _vm._v(" "), _c('div', {
+      staticClass: "col-sm-10"
+    }, [_c('div', {
       staticClass: "form-group"
     }, [_c('label', [_vm._v("Имя")]), _vm._v(" "), _c('input', {
       directives: [{
@@ -66194,7 +66200,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
         },
         expression: "review.text"
       }
-    })], 1)]), _vm._v(" "), _c('div', {
+    })], 1)])])]), _vm._v(" "), _c('div', {
       staticClass: "panel-footer"
     }, [_c('a', {
       staticClass: "btn btn-default",
--- resources/assets/js/components/pages/newpage/blocks/TextReviews.vue
+++ resources/assets/js/components/pages/newpage/blocks/TextReviews.vue
@@ -6,13 +6,20 @@
                     <div class="panel panel-primary">
                         <div class="panel-heading">{{ 'Отзыв ' + (index + 1) }}</div>
                         <div class="panel-body">
-                            <div class="form-group">
-                                <label>Имя</label>
-                                <input type="text" v-model="review.name" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label>Отзыв</label>
-                                <quill-editor v-model="review.text"></quill-editor>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    //
+                                </div>
+                                <div class="col-sm-10">
+                                    <div class="form-group">
+                                        <label>Имя</label>
+                                        <input type="text" v-model="review.name" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Отзыв</label>
+                                        <quill-editor v-model="review.text"></quill-editor>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="panel-footer"><a href="#" class="btn btn-default" @click.prevent="removeReview">Удалить отзыв</a></div>
