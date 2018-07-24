92880f4113f6f7e79b0c977747da1ac10a4371b2
begimov <begimov@gmail.com>
2018-07-24 14:39:50 +0300

    highestIdsFirst method added to RepositoryInterface

--- app/Repositories/RepositoryInterface.php
+++ app/Repositories/RepositoryInterface.php
@@ -7,6 +7,7 @@ interface RepositoryInterface
     public function get();
     public function filter($request);
     public function paginate($by);
+    public function highestIdsFirst();
     public function withTrashed();
     public function findById($id);
 }
