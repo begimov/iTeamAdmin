1686cacb86894932ca5b6666da9a139001e9ffff
begimov <begimov@gmail.com>
2018-08-13 16:00:12 +0300

    image block's template created

---
+++ resources/views/pages/page/blocks/image.blade.php
@@ -0,0 +1,10 @@
+<div class="row">
+  <div class="col">
+    <file-uploader 
+      v-model="data.files.img1" 
+      parent-resource-type="element"
+      maxFiles="1"
+      max-filesize=1>
+    </file-uploader>
+  </div>
+</div>
