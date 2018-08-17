a5a675f13559817a8cfd74169b78bf942bead42e
begimov <begimov@gmail.com>
2018-08-17 11:09:25 +0300

    Theme model class and related migration has been created

---
+++ app/Models/Pages/Theme.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Pages;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Theme extends Model
+{
+    //
+}
---
+++ database/migrations/2018_08_17_080512_create_themes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('themes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->string('name');
+            $table->string('slug')->nullable()->unique();
+
+            $table->foreign('parent_id')->references('id')->on('themes')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('themes');
+    }
+}
