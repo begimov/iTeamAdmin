24520009063962cd7aa0cc9d7c70ed0e5540cfc1
begimov <begimov@gmail.com>
2018-08-17 11:23:28 +0300

    ThemesTableSeeder has been created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(BlocksTableSeeder::class);
         $this->call(ResourceTypesTableSeeder::class);
+        $this->call(ThemesTableSeeder::class);
     }
 }
