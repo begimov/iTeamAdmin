99498fcbe0263743580946e2e534d689b700c036
begimov <begimov@gmail.com>
2018-08-21 16:22:44 +0300

    title block updated

--- database/seeds/BlocksTableSeeder.php
+++ database/seeds/BlocksTableSeeder.php
@@ -42,7 +42,7 @@ class BlocksTableSeeder extends Seeder
             ],
             [
                 'view' => 'title',
-                'data' => '{"text":"","subtext":""}'
+                'data' => '{"text":"","subtext":"","comment":""}'
             ],
             [
                 'view' => 'subtitle',
--- resources/views/pages/page/blocks/title.blade.php
+++ resources/views/pages/page/blocks/title.blade.php
@@ -1,12 +1,14 @@
 <div class="row">
   <div class="col-md-12">
     <p>
-      <!-- <quill-editor v-model="data.text" :options="{modules: {toolbar: [[{ 'align': [] }],[{ 'color': [] }]]}}"></quill-editor> -->
       <div class="form-group">
         <input type="text" v-model="data.text" placeholder="Заголовок..." class="form-control">
       </div>
       <div class="form-group">
         <input type="text" v-model="data.subtext" placeholder="Подзаголовок..." class="form-control">
+      </div>
+      <div class="form-group">
+        <input type="text" v-model="data.comment" placeholder="Подзаголовок..." class="form-control">
       </div>
     </p>  
   </div>
