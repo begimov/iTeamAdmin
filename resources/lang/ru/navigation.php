33dae14562aea65787a66813ce06b32ba07fea4f
begimov <begimov@gmail.com>
2018-09-05 09:43:03 +0300

    tests navigation link created

--- resources/lang/ru/navigation.php
+++ resources/lang/ru/navigation.php
@@ -5,6 +5,7 @@ return [
     'products' => 'Продукты',
     'materials' => 'Материалы',
     'pages' => 'Страницы',
+    'tests' => 'Тесты',
     'master_classes' => 'Мастер-классы',
     'create' => 'Создать',
 ];
