a25ad759a85aaf7e46b57cea8bff0058fb9d4505
begimov <begimov@gmail.com>
2018-09-21 13:14:39 +0300

    ReviewController created

---
+++ app/Http/Controllers/Reviews/ReviewController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Reviews;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReviewController extends Controller
+{
+    public function index()
+    {
+        return 'REVIEWS';
+    }
+}
