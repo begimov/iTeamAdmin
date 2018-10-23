aab2c89158564647aee90c743ea3689dbe61d89a
begimov <begimov@gmail.com>
2018-10-23 07:26:28 +0300

    Products category filter created

--- app/Filters/Products/ProductFilters.php
+++ app/Filters/Products/ProductFilters.php
@@ -3,11 +3,15 @@
 namespace App\Filters\Products;
 
 use App\Filters\FiltersAbstract;
-use App\Filters\Products\Product\SearchFilter;
+use App\Filters\Products\Product\{
+    SearchFilter,
+    CategoryFilter
+};
 
 class ProductFilters extends FiltersAbstract
 {
     protected $filters = [
         'searchQuery' => SearchFilter::class,
+        'categories' => CategoryFilter::class,
     ];
 }
