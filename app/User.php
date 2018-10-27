ccd99af1b89896bf4dc3523694a72bbbd6e35209
begimov <begimov@gmail.com>
2018-10-27 09:46:44 +0300

    returning default models from relationship methods in User and Order model classes

--- app/User.php
+++ app/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable
 
     public function userProfile()
     {
-        return $this->hasOne(UserProfile::class);
+        return $this->hasOne(UserProfile::class)->withDefault();
     }
 
     public function roles()
