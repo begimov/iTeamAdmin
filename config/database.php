757685d6c947d6103552a8b03f73ef1e26f2028a
begimov <begimov@gmail.com>
2018-10-31 11:16:38 +0300

    testing mysql connection added to config

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'iteam_testing',
+            'username' => 'homestead',
+            'password' => 'secret',
+            'unix_socket' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
